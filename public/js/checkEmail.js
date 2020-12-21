9c61f5cfef2e4855abb9470c312bf7d147b25e26
antrikshmisri <antrikshmisri@gmail.com>
2020-12-21 17:35:18 +0530

    feat: added submit btn disable lagic

--- public/js/checkEmail.js
+++ public/js/checkEmail.js
@@ -1,8 +1,11 @@
 function checkEmail(email , err) {
+
+    const signinbtn = document.querySelector('#signin-button')
+    const registerbtn = document.querySelector('#register')
     const userEmail = document.querySelector(`#${email}`).value;
     let userEmailFormate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     let flag;
-
+    
     if (userEmail.match(userEmailFormate)) {
       flag = false;
     } else {
@@ -11,9 +14,29 @@ function checkEmail(email , err) {
 
     if (flag) {
       document.getElementById(err).style.display = "block";
+      if(signinbtn)
+      {
+        signinbtn.classList.remove('active')
+        signinbtn.classList.add('disabled')
+      }
+      if(registerbtn)
+      {
+        registerbtn.classList.remove('active')
+        registerbtn.classList.add('disabled')
+      }
       // console.log(flag);
     } else {
       document.getElementById(err).style.display = "none";
+      if(signinbtn)
+      {
+        signinbtn.classList.remove('disabled')
+        signinbtn.classList.add('active')
+      }
+      if(registerbtn)
+      {
+        registerbtn.classList.remove('disabled')
+        registerbtn.classList.add('active')
+      }
       // console.log(flag);
     }
   }
