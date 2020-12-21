86cb5ad0dd4fb59ee2c7f63ed7913015baf3bcbf
antrikshmisri <antrikshmisri@gmail.com>
2020-12-21 17:35:53 +0530

    feat: added submit btn disable logic

--- public/js/checkPass.js
+++ public/js/checkPass.js
@@ -1,5 +1,7 @@
 function checkPass(pass , err)
 {
+    const signinbtn = document.querySelector('#signin-button')
+    const registerbtn = document.querySelector('#register')
     let userPassword = document.getElementById(pass);
     let userPasswordFormulate = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}/;
     let flag;
@@ -12,7 +14,27 @@ function checkPass(pass , err)
   
     if (flag) {
       document.getElementById(err).style.display = "block";
+      if(signinbtn)
+      {
+        signinbtn.classList.remove('active')
+        signinbtn.classList.add('disabled')
+      }
+      if(registerbtn)
+      {
+        registerbtn.classList.remove('active')
+        registerbtn.classList.add('disabled')
+      }
     } else {
       document.getElementById(err).style.display = "none";
+      if(signinbtn)
+      {
+        signinbtn.classList.remove('disabled')
+        signinbtn.classList.add('active')
+      }
+      if(registerbtn)
+      {
+        registerbtn.classList.remove('disabled')
+        registerbtn.classList.add('active')
+      }
     }
 }
